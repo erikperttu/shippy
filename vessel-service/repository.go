154feab61faea71a7e2366894d8b6e98cb203525
Erik Perttu <erik.perttu@gmail.com>
2017-12-12 17:44:05 +0700

    WIP tutorial 3

--- vessel-service/repository.go
+++ vessel-service/repository.go
@@ -1,1 +1,7 @@
 package main
+
+import pb "github.com/erikperttu/shippy/vessel-service/proto/vessel"
+
+func (repo *VesselRepository) Create(vessel *pb.Vessel) error {
+	return repo.collection().Insert(vessel)
+}
