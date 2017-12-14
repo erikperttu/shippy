b0d54ec93ea3545184a6e3c25ba974cc624bb9b4
Erik Perttu <erik.perttu@gmail.com>
2017-12-14 11:46:27 +0700

    user service work

--- user-service/handler.go
+++ user-service/handler.go
@@ -1,7 +1,21 @@
 package main
 
 import (
-	"golang.org/x/net/context"
 	pb "github.com/erikperttu/shippy/user-service/proto/user"
+	"golang.org/x/net/context"
 )
 
+type service struct {
+	repo         Repository
+	tokenService Authable
+}
+
+func (srv *service) Get(ctx context.Context, req *pb.User, res *pb.Response) error {
+	user, err := srv.repo.Get(req.Id)
+	if err != nil {
+		return err
+	}
+
+	res.User = user
+	return nil
+}
