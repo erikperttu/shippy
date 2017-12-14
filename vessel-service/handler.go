f4c1983bd3f44b9ec8dca454151ec2b089a0c4a9
Erik Perttu <erik.perttu@gmail.com>
2017-12-14 11:45:57 +0700

    refactor vessel-service

--- vessel-service/handler.go
+++ vessel-service/handler.go
@@ -6,21 +6,31 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-
 // Our gRPC service handler
 type service struct {
 	session *mgo.Session
 }
 
-
 func (s *service) GetRepo() Repository {
 	return &VesselRepository{s.session.Clone()}
 }
 
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+	defer s.GetRepo().Close()
+	// Find the next available vessel
+	vessel, err := s.GetRepo().FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
+
 func (s *service) Create(ctx context.Context, req *pb.Vessel, res *pb.Response) error {
-	repo := s.GetRepo()
-	defer repo.Close()
-	if err := repo.Create(req); err != nil {
+	defer s.GetRepo().Close()
+	if err := s.GetRepo().Create(req); err != nil {
 		return err
 	}
 	res.Vessel = req
--- vessel-service/repository.go
+++ vessel-service/repository.go
@@ -1,7 +1,55 @@
 package main
 
-import pb "github.com/erikperttu/shippy/vessel-service/proto/vessel"
+import (
+	pb "github.com/erikperttu/shippy/vessel-service/proto/vessel"
+	"github.com/ewanvalentine/shippy/vessel-service/proto/vessel"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	dbName           = "shippy"
+	vesselCollection = "vessels"
+)
+
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+	Create(vessel *pb.Vessel) error
+	Close()
+}
+
+type VesselRepository struct {
+	session *mgo.Session
+}
+
+// FindAvailable - checks a specification against a map of vessels,
+// if capacity and max weight are below a vessels capacity and max weight,
+// then return that vessel.
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	var vessel *pb.Vessel
+
+	// Here we define a bit of a more complex query than the consignment-service
+	// GetAll function. We are asking for a vessels with max weight and capacity
+	// => to the given capacity and weight
+	// We are also using the 'One" function here since that is all we want
+	err := repo.collection().Find(bson.M{
+		"capacity":  bson.M{"$gte": spec.Capacity},
+		"maxweight": bson.M{"$gte": spec.MaxWeight},
+	}).One(&vessel)
+	if err != nil {
+		return nil, err
+	}
+	return vessel, nil
+}
+
+func (repo *VesselRepository) Close() {
+	repo.session.Close()
+}
 
 func (repo *VesselRepository) Create(vessel *pb.Vessel) error {
 	return repo.collection().Insert(vessel)
+}
+
+func (repo *VesselRepository) collection() *mgo.Collection {
+	return repo.session.DB(dbName).C(vesselCollection)
 }
